db5e1c29810303ac9357e4d53dd20376d7eeadfc
Wout Bittremieux <bittremieux@gmail.com>
2014-01-11 10:32:27 +0100

    Test validation during marshalling
    Limited test: only for duplicate ID’s and missing content. However, the
    general usage is tested; other validations of the XML schema will
    therefore be caught as well.

--- src/test/java/inspector/jqcml/io/xml/QcMLWriterTest.java
+++ src/test/java/inspector/jqcml/io/xml/QcMLWriterTest.java
@@ -5,6 +5,9 @@ import java.io.File;
 import inspector.jqcml.io.QcMLWriter;
 import inspector.jqcml.model.Cv;
 
+import inspector.jqcml.model.QcML;
+import inspector.jqcml.model.QualityAssessment;
+import inspector.jqcml.model.QualityParameter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,5 +34,36 @@ public class QcMLWriterTest {
     @Test(expected = UnsupportedOperationException.class)
     public void writeCv_valid() {
         writer.writeCv(new Cv("name", "uri", "id"));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void writeQcML_duplicateID() {
+        QcML qcml = new QcML();
+        qcml.setFileName("Invalid.qcML");
+
+        Cv cv = new Cv("name", "uri", "id");
+        qcml.addCv(cv);
+
+        QualityAssessment run = new QualityAssessment("id");
+        QualityParameter param = new QualityParameter("name", cv, "id");
+        param.setAccession("accession");
+        run.addQualityParameter(param);
+        qcml.addRunQuality(run);
+
+        writer.writeQcML(qcml);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void writeQcML_missingContent() {
+        QcML qcml = new QcML();
+        qcml.setFileName("Invalid.qcML");
+
+        QualityAssessment run = new QualityAssessment("run");
+        QualityParameter param = new QualityParameter("name", new Cv("name", "uri", "cv"), "param");
+        param.setAccession("accession");
+        run.addQualityParameter(param);
+        qcml.addRunQuality(run);
+
+        writer.writeQcML(qcml);
     }
 }
